409420ba916d209cfd6138172602b5645a4e7308
Nick Anderssohn <ncadevelopment@gmail.com>
2016-10-14 01:52:44 -0700

    can draw crop box now.

--- web/image_editor.dart
+++ web/image_editor.dart
@@ -1,17 +1,49 @@
 import 'dart:html';
-
+import 'dart:math';
 
 class ImageEditor {
   CanvasElement canvas;
   CanvasRenderingContext2D ctx;
+  CanvasElement drawLayer = new CanvasElement();
+  CanvasRenderingContext2D get drawLayerCtx => drawLayer.context2D;
+  CanvasElement imageLayer = new CanvasElement();
+  CanvasRenderingContext2D get imageLayerCtx => imageLayer.context2D;
   ImageElement image;
   File imgFile;
+  bool cropping = false;
+  bool drawingBox = false;
+  bool croppingStarted = false;
+  Point p1 = null; //crop box point 1
+  Point p2 = null; //crop box point 2
+  Point p3 = null; //crop box point 3
+  Point p4 = null; //crop box point 4
+  double scalar = 1.0;
+  int get canvasWidth => canvas.width;
+  void set canvasWidth(int value) {
+    canvas.width = value;
+    drawLayer.width = value;
+    imageLayer.width = value;
+    scalar = image.width / canvas.getBoundingClientRect().width;
+  }
+  int get canvasHeight => canvas.height;
+  void set canvasHeight(int value) {
+    canvas.height = value;
+    drawLayer.height = value;
+    imageLayer.height = value;
+  }
 
   ImageEditor(CanvasElement canvas, File imageFile) {
     this.canvas = canvas;
     ctx = canvas.context2D;
     imgFile = imageFile;
     _loadFile();
+    _handleCropping();
+  }
+
+  _refreshDisplay() {
+    ctx.clearRect(0, 0, canvasWidth, canvasHeight);
+    ctx.drawImage(imageLayer, 0, 0);
+    ctx.drawImage(drawLayer, 0, 0);
   }
 
   _loadFile() {
@@ -23,12 +55,75 @@ class ImageEditor {
           canvas.style.width = image.width.toString() + 'px';
         else
           canvas.style.width = "100%";
-        canvas.width = image.width;
-        canvas.height = (image.height * (canvas.width / image.width)).round(); //scale correctly
-        ctx.drawImageScaled(image, 0, 0, canvas.width, canvas.height);
+        canvasWidth = image.width;
+        //canvasHeight = (image.height * (canvas.width / image.width)).round(); //scale correctly
+        canvasHeight = image.height;
+        imageLayerCtx.drawImageScaled(image, 0, 0, canvas.width, canvas.height);
+        _refreshDisplay();
       });
       image.src = e.target.result;
     });
     reader.readAsDataUrl(imgFile);
+  }
+
+  _handleCropping() {
+    Point startP = null;
+    canvas.onMouseDown.listen((MouseEvent e) {
+      if (cropping) {
+        //if box coordinates have not been set
+        if (startP == null) {
+          var cRect = canvas.getBoundingClientRect();
+          //instantiate all 4 points at current location
+          startP = new Point(e.client.x - cRect.left, e.client.y - cRect.top);
+        }
+      }
+    });
+
+    canvas.onMouseMove.listen((var e) {
+      if (cropping) {
+        if (startP != null) {
+          if (!croppingStarted) { //drawing initial box....
+            var cRect = canvas.getBoundingClientRect();
+            _drawBox(startP, new Point(e.client.x - cRect.left, e.client.y - cRect.top)); //refreshes display in this function as well
+          }
+        }
+      }
+    });
+
+    canvas.onMouseUp.listen((var e) {
+      if (cropping) {
+        if (startP != null) {
+          if (!croppingStarted) {
+            var cRect = canvas.getBoundingClientRect();
+            int width = startP.x - (e.client.x - cRect.left);
+            int height = startP.y - (e.client.y - cRect.top);
+            croppingStarted = true;
+            p1 = startP;
+            p2 = new Point(startP.x + width, startP.y);
+            p3 = new Point(startP.x + width, startP.y + height);
+            p4 = new Point(startP.x, startP.y + height);
+          }
+        }
+      }
+    });
+  }
+
+//accepts 2 points diagonal from each other
+  _drawBox(Point d1, Point d2) {
+    int width = d2.x - d1.x;
+    int height = d2.y - d1.y;
+    drawLayerCtx.clearRect(0, 0, canvasWidth, canvasHeight);
+
+    drawLayerCtx.beginPath();
+    drawLayerCtx.moveTo(d1.x * scalar, d1.y * scalar);
+    drawLayerCtx.lineTo(d1.x * scalar, (d1.y + height) * scalar);
+    drawLayerCtx.lineTo((d1.x + width) * scalar, (d1.y + height) * scalar);
+    drawLayerCtx.lineTo((d1.x + width) * scalar, d1.y * scalar);
+    drawLayerCtx.lineTo(d1.x * scalar, d1.y * scalar);
+    drawLayerCtx.stroke();
+    drawLayerCtx.closePath();
+
+
+    _refreshDisplay();
   }
 }
--- web/main.dart
+++ web/main.dart
@@ -1,25 +1,31 @@
 import 'dart:html';
 import 'image_editor.dart';
+
 Element titleElement;
 CanvasElement picCanvas;
 InputElement fileInput;
 ImageEditor editor;
+ButtonElement btnCrop;
 
 main() {
   _getElements();
   _updateStyles();
-  fileInput.onChange.listen((var e) {
-    editor = new ImageEditor(picCanvas, fileInput.files[0]);
-  });
+  _setHandlers();
 }
 
 _getElements() {
   titleElement = querySelector('#title');
   picCanvas = querySelector('#pic-canvas');
   fileInput = querySelector('#file-input');
+  btnCrop = querySelector('#btn-crop');
 }
 
 _updateStyles() {
   titleElement.classes.add('title');
   picCanvas.classes.add('pic-canvas');
+}
+
+_setHandlers() {
+  fileInput.onChange.listen((var e) => editor = new ImageEditor(picCanvas, fileInput.files[0]));
+  btnCrop.onClick.listen((var e) => editor.cropping = !editor.cropping);
 }
