889481aba5e5bcb3abd7aaf99f9390cb5ff408d8
Nick Anderssohn <ncadevelopment@gmail.com>
2016-10-13 23:21:03 -0700

    if pic width less than window width, canvas will be set to that width.

--- web/image_editor.dart
+++ web/image_editor.dart
@@ -19,6 +19,10 @@ class ImageEditor {
     image = new ImageElement();
     reader.onLoad.listen((var e) {
       image.onLoad.listen((var e) {
+        if (image.width < window.innerWidth)
+          canvas.style.width = image.width.toString() + 'px';
+        else
+          canvas.style.width = "100%";
         canvas.width = image.width;
         canvas.height = (image.height * (canvas.width / image.width)).round(); //scale correctly
         ctx.drawImageScaled(image, 0, 0, canvas.width, canvas.height);
