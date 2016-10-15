bb46c47db4f5607e509118e99aa66d7bac8df0ab
Nick Anderssohn <ncadevelopment@gmail.com>
2016-10-14 20:02:20 -0700

    fixed dragging corners of crop box.

--- web/image_editor.dart
+++ web/image_editor.dart
@@ -1,5 +1,6 @@
 import 'dart:html';
 import 'dart:math';
+import 'r_point.dart';
 
 class ImageEditor {
   CanvasElement canvas;
@@ -10,13 +11,24 @@ class ImageEditor {
   CanvasRenderingContext2D get imageLayerCtx => imageLayer.context2D;
   ImageElement image;
   File imgFile;
-  bool cropping = false;
-  bool croppingStarted = false;
-  bool draggingCorner = false;
-  Point p1 = null; //crop box point 1
-  Point p2 = null; //crop box point 2
-  Point p3 = null; //crop box point 3
-  Point p4 = null; //crop box point 4
+  bool _cropping = false;
+  bool get cropping => _cropping;
+  void set cropping(bool value) {
+    if (!_cropping && value == true) {
+      //add/draw points
+      p1 = new RPoint((100 * scalar).round(), (100 * scalar).round());
+      p2 = new RPoint((200 * scalar).round(), (100 * scalar).round());
+      p3 = new RPoint((200 * scalar).round(), (200 * scalar).round());
+      p4 = new RPoint((100 * scalar).round(), (200 * scalar).round());
+      _drawBox();
+    }
+    _cropping = value;
+  }
+  RPoint pSelected = null;
+  RPoint p1 = null; //crop box point 1
+  RPoint p2 = null; //crop box point 2
+  RPoint p3 = null; //crop box point 3
+  RPoint p4 = null; //crop box point 4
   double scalar = 1.0;
   int get canvasWidth => canvas.width;
 
@@ -26,7 +38,7 @@ class ImageEditor {
     imageLayer.width = value;
     scalar = image.width / canvas.getBoundingClientRect().width;
   }
-  
+
   int get canvasHeight => canvas.height;
 
   void set canvasHeight(int value) {
@@ -70,124 +82,95 @@ class ImageEditor {
   }
 
   _handleCropping() {
-    Point startP = null;
     canvas.onMouseDown.listen((MouseEvent e) {
       if (cropping) {
         var cRect = canvas.getBoundingClientRect();
-        int realX = e.client.x - cRect.left;
-        int realY = e.client.y - cRect.top;
+        int realX = (e.client.x - cRect.left) * scalar;
+        int realY = (e.client.y - cRect.top) * scalar;
         //if box coordinates have not been set
-        if (startP == null)
-          startP = new Point(realX, realY);
-        else if (_getSelectedCropCorner(realX, realY) != null)
-          draggingCorner = true;
+        pSelected = _getSelectedCropCorner(realX, realY);
+        print(pSelected);
       }
-      print("dragging corner: " + draggingCorner.toString());
     });
 
     canvas.onMouseMove.listen((var e) {
       if (cropping) {
-        if (startP != null) {
+        if (pSelected != null) {
           var cRect = canvas.getBoundingClientRect();
-          int realX = e.client.x - cRect.left;
-          int realY = e.client.y - cRect.top;
-          if (!croppingStarted) { //drawing initial box....
-
-            _drawBox(startP, new Point(realX, realY)); //refreshes display in this function as well
-          } else if (draggingCorner) {
-            var points = _getSelectedCropCorner(realX, realY);
-            _drawBox(points[0], points[1]);
-          }
+          int realX = (e.client.x - cRect.left) * scalar;
+          int realY = (e.client.y - cRect.top) * scalar;
+          pSelected.x = realX;
+          pSelected.y = realY;
+          _drawBox();
         }
       }
     });
 
     canvas.onMouseUp.listen((var e) {
       if (cropping) {
-        if (startP != null) {
-          if (!croppingStarted) {
-            var cRect = canvas.getBoundingClientRect();
-            int width = startP.x - (e.client.x - cRect.left);
-            int height = startP.y - (e.client.y - cRect.top);
-            croppingStarted = true;
-            p1 = startP;
-            p2 = new Point(startP.x + width, startP.y);
-            p3 = new Point(startP.x + width, startP.y + height);
-            p4 = new Point(startP.x, startP.y + height);
-          }
-        }
-        draggingCorner = false;
+        pSelected = null;
       }
     });
   }
 
-  List<Point> _getSelectedCropCorner(int realX, int realY) {
-    print("p1.x: " + p1.x.toString());
-    //print("p1.x * scalar: " + (p1.x * scalar).toString());
-    print("p2.x: " + p2.x.toString());
-    //print("p1.x * scalar: " + (p1.x * scalar).toString());
-    print("p3.x: " + p3.x.toString());
-    //print("p1.x * scalar: " + (p1.x * scalar).toString());
-    print("p4.x: " + p4.x.toString());
-    //print("p1.x * scalar: " + (p1.x * scalar).toString());
-    print("realX: " + realX.toString());
-    //print("realX * scalar: " + (realX * scalar).toString());
-    print("p1.y: " + p1.y.toString());
-    //print("p1.x * scalar: " + (p1.x * scalar).toString());
-    print("p2.y: " + p2.y.toString());
-    //print("p1.x * scalar: " + (p1.x * scalar).toString());
-    print("p3.y: " + p3.y.toString());
-    //print("p1.x * scalar: " + (p1.x * scalar).toString());
-    print("p4.y: " + p4.y.toString());
-    //print("p1.x * scalar: " + (p1.x * scalar).toString());
-    print("realY: " + realY.toString());
-    //print("realX * scalar: " + (realX * scalar).toString());
-    if (realX > p1.x - 10 && realX < p1.x + 10 && realY > p1.y - 10 && realY < p1.y + 10)
-      return [p1, p3];
-    if (realX > p2.x - 10 && realX < p2.x + 10 && realY > p2.y - 10 && realY < p2.y + 10)
-      return [p2, p4];
-    if (realX > p3.x - 10 && realX < p3.x + 10 && realY > p3.y - 10 && realY < p3.y + 10)
-      return [p3, p1];
-    if (realX > p4.x - 10 && realX < p4.x + 10 && realY > p4.y - 10 && realY < p4.y + 10)
-      return [p4, p2];
+  RPoint _getSelectedCropCorner(int realX, int realY) {
+    if (realX > (p1.x + 2 * scalar) - 4 * scalar && realX < (p1.x + 2 * scalar) + 4 * scalar && realY > (p1.y + 2 * scalar) - 4 * scalar && realY < (p1.y + 2 * scalar) + 4 * scalar)
+      return p1;
+    if (realX > (p2.x + 2 * scalar) - 4 * scalar && realX < (p2.x + 2 * scalar) + 4 * scalar && realY > (p2.y + 2 * scalar) - 4 * scalar && realY < (p2.y + 2 * scalar) + 4 * scalar)
+      return p2;
+    if (realX > (p3.x + 2 * scalar) - 4 * scalar && realX < (p3.x + 2 * scalar) + 4 * scalar && realY > (p3.y + 2 * scalar) - 4 * scalar && realY < (p3.y + 2 * scalar) + 4 * scalar)
+      return p3;
+    if (realX > (p4.x + 2 * scalar) - 4 * scalar && realX < (p4.x + 2 * scalar) + 4 * scalar && realY > (p4.y + 2 * scalar) - 4 * scalar && realY < (p4.y + 2 * scalar) + 4 * scalar)
+      return p4;
 
     return null;
   }
 
-//accepts 2 points diagonal from each other
-  _drawBox(Point d1, Point d2) {
-    int width = d2.x - d1.x;
-    int height = d2.y - d1.y;
-    drawLayerCtx.clearRect(0, 0, canvasWidth, canvasHeight);
+  _drawBox() {
+      if (pSelected == p1) {
+        p2.y = p1.y;
+        p4.x = p1.x;
+      } else if (pSelected == p2) {
+        p1.y = p2.y;
+        p3.x = p2.x;
+      } else if (pSelected == p3) {
+        p4.y = p3.y;
+        p2.x = p3.x;
+      } else if (pSelected == p4) {
+        p3.y = p4.y;
+        p1.x = p4.x;
+      }
 
-    //draw dots at corners
-    drawLayerCtx.beginPath();
-    drawLayerCtx.arc(d1.x * scalar, d1.y * scalar, 4, 0, 2 * PI);
-    drawLayerCtx.fill();
-    drawLayerCtx.closePath();
-    drawLayerCtx.beginPath();
-    drawLayerCtx.arc(d1.x * scalar, (d1.y + height) * scalar, 4, 0, 2 * PI);
-    drawLayerCtx.fill();
-    drawLayerCtx.closePath();
-    drawLayerCtx.beginPath();
-    drawLayerCtx.arc((d1.x + width) * scalar, (d1.y + height) * scalar, 4, 0, 2 * PI);
-    drawLayerCtx.fill();
-    drawLayerCtx.closePath();
-    drawLayerCtx.beginPath();
-    drawLayerCtx.arc((d1.x + width) * scalar, d1.y * scalar, 4, 0, 2 * PI);
-    drawLayerCtx.fill();
-    drawLayerCtx.closePath();
+      drawLayerCtx.clearRect(0, 0, canvasWidth, canvasHeight);
 
-    //draw box
-    drawLayerCtx.beginPath();
-    drawLayerCtx.moveTo(d1.x * scalar, d1.y * scalar);
-    drawLayerCtx.lineTo(d1.x * scalar, (d1.y + height) * scalar);
-    drawLayerCtx.lineTo((d1.x + width) * scalar, (d1.y + height) * scalar);
-    drawLayerCtx.lineTo((d1.x + width) * scalar, d1.y * scalar);
-    drawLayerCtx.lineTo(d1.x * scalar, d1.y * scalar);
-    drawLayerCtx.stroke();
-    drawLayerCtx.closePath();
+      //draw dots at corners
+      drawLayerCtx.beginPath();
+      drawLayerCtx.arc(p1.x, p1.y, 4 * scalar, 0, 2 * PI);
+      drawLayerCtx.fill();
+      drawLayerCtx.closePath();
+      drawLayerCtx.beginPath();
+      drawLayerCtx.arc(p2.x, p2.y, 4 * scalar, 0, 2 * PI);
+      drawLayerCtx.fill();
+      drawLayerCtx.closePath();
+      drawLayerCtx.beginPath();
+      drawLayerCtx.arc(p3.x, p3.y, 4 * scalar, 0, 2 * PI);
+      drawLayerCtx.fill();
+      drawLayerCtx.closePath();
+      drawLayerCtx.beginPath();
+      drawLayerCtx.arc(p4.x, p4.y, 4 * scalar, 0, 2 * PI);
+      drawLayerCtx.fill();
+      drawLayerCtx.closePath();
 
-    _refreshDisplay();
-  }
+      //draw box
+      drawLayerCtx.beginPath();
+      drawLayerCtx.moveTo(p1.x, p1.y);
+      drawLayerCtx.lineTo(p2.x, p2.y);
+      drawLayerCtx.lineTo(p3.x, p3.y);
+      drawLayerCtx.lineTo(p4.x, p4.y);
+      drawLayerCtx.lineTo(p1.x, p1.y);
+      drawLayerCtx.stroke();
+      drawLayerCtx.closePath();
+
+      _refreshDisplay();
+    }
 }
