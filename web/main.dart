cb14acec2bdf6a121d78b814c5d4e9aef054c7bd
Nick Anderssohn <ncadevelopment@gmail.com>
2016-10-18 20:45:17 -0700

    instantiates new canvas everytime a new image is loaded.

--- web/main.dart
+++ web/main.dart
@@ -7,6 +7,7 @@ ImageEditor editor;
 DivElement divDownload;
 DivElement optionCrop;
 DivElement divSelectFile;
+DivElement canvasContainer;
 
 main() {
   _getElements();
@@ -19,15 +20,24 @@ _getElements() {
   optionCrop = querySelector('#option-crop');
   divDownload = querySelector('#option-download');
   divSelectFile = querySelector('#option-select-file');
+  canvasContainer = querySelector('#canvas-container');
 }
 
 
 _setHandlers() {
   divSelectFile.onClick.listen((var e) => document.getElementById('file-input').click());
-  fileInput.onChange.listen((var e) => editor = new ImageEditor(picCanvas, fileInput.files[0]));
+  fileInput.onChange.listen((var e) => _loadFile());
   optionCrop.onClick.listen((var e) => editor.cropBox.cropping = !editor.cropBox.cropping);
   divDownload.onClick.listen((var e) {
     var dataURL = editor.canvas.toDataUrl();
     window.location.href = dataURL;
   });
+}
+
+_loadFile() {
+  picCanvas.remove();
+  picCanvas = new CanvasElement();
+  canvasContainer.children.add(picCanvas);
+  picCanvas.classes.add('pic-canvas');
+  editor = new ImageEditor(picCanvas, fileInput.files[0]);
 }
