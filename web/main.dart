f0870c36a74987d990efcea8d811e60cb752f722
Nick Anderssohn <ncadevelopment@gmail.com>
2016-10-18 22:23:06 -0700

    drop down, revert to original, and download now downloads instead of opening link to pic.

--- web/main.dart
+++ web/main.dart
@@ -1,5 +1,6 @@
 import 'dart:html';
 import 'image_editor.dart';
+import 'drop_down.dart';
 
 CanvasElement picCanvas;
 InputElement fileInput;
@@ -7,20 +8,30 @@ ImageEditor editor;
 DivElement divDownload;
 DivElement optionCrop;
 DivElement divSelectFile;
+DivElement divEdit;
 DivElement canvasContainer;
+DropDown editDD;
+DivElement restoreOriginalOption;
+AreaElement downloadHelper;
 
 main() {
-  picCanvas = null;
   _getElements();
   _setHandlers();
 }
 
 _getElements() {
+  picCanvas = null;
   fileInput = querySelector('#file-input');
   optionCrop = querySelector('#option-crop');
   divDownload = querySelector('#option-download');
   divSelectFile = querySelector('#option-select-file');
+  divEdit = querySelector('#option-edit');
   canvasContainer = querySelector('#canvas-container');
+  downloadHelper = querySelector('#download-helper');
+
+  editDD = new DropDown(divEdit);
+  restoreOriginalOption = new DivElement();
+  restoreOriginalOption.text = 'Restore Original';
 }
 
 
@@ -30,8 +41,14 @@ _setHandlers() {
   optionCrop.onClick.listen((var e) => editor.cropBox.cropping = !editor.cropBox.cropping);
   divDownload.onClick.listen((var e) {
     var dataURL = editor.canvas.toDataUrl();
-    window.location.href = dataURL;
+    downloadHelper.href = dataURL;
+    print("test1");
+    document.getElementById('download-helper').click();
+    //window.location.href = dataURL;
   });
+  restoreOriginalOption.onClick.listen((var e) => _loadFile());
+  divEdit.onClick.listen((var e) => editDD.target.hidden = !editDD.target.hidden);
+  editDD.addItem(restoreOriginalOption);
 }
 
 _loadFile() {
