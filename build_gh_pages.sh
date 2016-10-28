8fc681dbe61498df7075ca0729ffa116d8b66ee9
Nick Anderssohn <ncadevelopment@gmail.com>
2016-10-27 23:55:12 -0700

    updated build script

--- build_gh_pages.sh
+++ build_gh_pages.sh
@@ -1,8 +1,10 @@
 #!/bin/bash
 pub get
 pub build
-rm -rf ~/pic-edit-online-gh-pages/
+rm -rf ~/pic-edit-online-gh-pages
+mkdir ~/pic-edit-online-gh-pages
 cp -R build/ ~/pic-edit-online-gh-pages
-cp web/main.dart ~/pic-edit-online-gh-pages/bulid/web/
+cp web/main.dart ~/pic-edit-online-gh-pages/build/web/
 git checkout gh-pages
-cp -R ~/pic-edit-online-gh-pages/build/ .
+rm -rf build
+cp -R ~/pic-edit-online-gh-pages/build/ ./build/
